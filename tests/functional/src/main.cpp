2070d06da045fb18141c508dd7ba01e1bddb61c6
Neisan <nathan.maillot@epitech.eu>
2024-06-04 15:32:18 +0400

    refactor: change vector in GUIManager by a mapp

--- tests/functional/src/main.cpp
+++ tests/functional/src/main.cpp
@@ -25,13 +25,11 @@ int main()
         gui->_menuBar->add(new EGE::Menu("View"));
         gui->_menuBar->add(new EGE::Menu("Help"));
 
-        gui->_panels.at(0)->add(new EGE::Button("Button", [](){std::cout << "PD" << std::endl;}));
-        gui->_panels.at(0)->add(new EGE::Text("Salut a tous bande de gentilles personnes..."));
-
+        gui->_panels["Main"]->add(new EGE::Button("Button", [](){std::cout << "PD" << std::endl;}));
+        gui->_panels["Main"]->add(new EGE::Text("Salut a tous bande de gentilles personnes..."));
 
         EGE::Camera camera(EGE::Maths::Vector3<float>(6.0f, 0.0f, 6.0f), EGE::Maths::Vector3<float>(0.0f, 1.0f, 0.0f), -135.0f, 0.0f);
         // EGE::Shader shader("/home/rStraif/delivery/TEK2/ETIB/etib-game-engine/assets/shader/vertex.vert", "/home/rStraif/delivery/TEK2/ETIB/etib-game-engine/assets/shader/fragment.frag");
-
 
         window->bindTrigger(EGE::Event::Trigger(EGE::Event::Keyboard, EGE::Event::Key::KeyW, EGE::Event::Pressed, [&camera]() {
             camera.move(EGE::Camera::FORWARD, 0.1f);
